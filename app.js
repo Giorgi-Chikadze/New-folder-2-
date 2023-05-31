3f6f5b0a66666cb24a718b2a406a11fc0d549a05
Giorgi-Chikadze <131460016+Giorgi-Chikadze@users.noreply.github.com>
2023-05-31 15:44:29 +0400

    changed style.css

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("dagi")
+
+let burger = document.getElementById("burger");
+let nav = document.getElementById("navigation");
+
+burger.addEventListener("click", function () {
+    if (nav.style.display === "none") {
+        nav.style.display === "block"
+    } else {
+        nav.style.display === "none"
+    }
+})
